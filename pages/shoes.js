100a37aa852e63803bbc9a3ae0c6fd5e53cccae2
Harshit0523 <harshupahade2004@gmail.com>
2023-07-22 03:11:22 +0530

    api fetched successfully

--- pages/shoes.js
+++ pages/shoes.js
@@ -2,126 +2,56 @@ import Link from "next/link";
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import image1 from "../public/images/image1.jpg";
-import image2 from "../public/images/image12.jpg";
+
 // import Middle from "@/components/middle";
 import Image from "next/image";
 const shoes = (props) => {
   return (
     <div class="bg-white">
       <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="sr-only">{props.products}</h2>
+        <h2 class="sr-only"></h2>
 
         <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
-                alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg"
-                alt="Olive drab green insulated bottle with flared screw lid and flat top."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg"
-                alt="Person using a pen to cross a task off a productivity paper card."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
-                alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">
-              Machined Mechanical Pencil
-            </h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
-                alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">
-              Machined Mechanical Pencil
-            </h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
-                alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">
-              Machined Mechanical Pencil
-            </h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
-                alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">
-              Machined Mechanical Pencil
-            </h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-          </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
-                alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">
-              Machined Mechanical Pencil
-            </h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-          </a>
+          {props.products.data.map((item) => {
+            return (
+              <a href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <Image
+                    height={200}
+                    width={200}
+                    src={item.attributes.thumbnail.data.attributes.name}
+                    alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                    // class="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700">
+                  {item.attributes.name}
+                </h3>
+                <p class="mt-1 text-lg font-medium text-gray-900">
+                  {item.attributes.price}
+                </p>
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
   );
 };
 export async function getServerSideProps(context) {
-  let headers = {Authorization : "Bearer 2b37bc71bff4d9adc593d3069020aeecfec20f607696ce38a6de20bb98ed9167077eb3eeac0beaddd9e083926b1fe0e13ca7a117af61b3397935a05eb81bb56a4c4de88ecfd771f2140ae22b10d9630d3165c2d037c8ce5fa4c638008a9512c11a9cf312520d32859c7b14ba26c8cb3506e73edae64837a3825a2d3cbc060c79"}
- let a = await fetch("http://localhost:1337/api/products?populate=*",{headers:headers})
- let products = await a.json()
- return {
-   props:{products:products},
- }
+  let headers = {
+    Authorization:
+      "Bearer f2dc86acfb78c950fcbde0fa39547d40c9bcf401cba8e7220cf36d2a4e2acdcb6f77fe55ab5f0a49082553af4ea5fd40dceb57af5291e65377e5cf46ff157213fc5ecd20d806f3f72411840448f9f8ef8ac444466413cb32c390efd3773be47262df88a6f681e86d93fbcf0eca7eb8fd9ba5938a4e804165c5e5cf489374baa3",
+  };
+  console.log("before");
+  let a = await fetch("http://localhost:1337/api/products?populate=*", {
+    headers: headers,
+  });
+  console.log("fatched");
+  let products = await a.json();
+  console.log(products);
+  return {
+    props: { products: products },
+  };
 }
 export default shoes;
